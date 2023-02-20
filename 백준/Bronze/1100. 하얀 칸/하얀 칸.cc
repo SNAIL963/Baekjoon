d3461367bdf16aece18a0d282c8ddd9ca7931b1e
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2023-02-21 02:24:18 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main() {
+	char chess[9][8];
+	int count = 0;
+
+	for (int i = 0; i < 8; i++) {
+		scanf("%s", chess[i]);
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if ((i + j) % 2 == 0 && chess[i][j] == 'F')
+				count += 1;
+		}
+	}
+	printf("%d", count);
+}
