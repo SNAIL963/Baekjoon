3fa0b197fcf06f92b9b63b11e6ecf67693835280
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2023-02-21 02:45:53 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+int main() {
+	int num_in[3], num_out[3];
+	scanf("%d %d %d", &num_in[0], &num_in[1], &num_in[2]);
+	int min = num_in[0];
+	int min_index = 0;
+	for (int i = 0; i < 3; i++) {
+		if (num_in[i] < min) {
+			min = num_in[i];
+			min_index = i;
+		}
+	}
+	num_out[0] = min;
+	for (int i = 0; i < 3; i++) {
+		if (i < min_index)
+			num_out[i + 1] = num_in[i];
+		else if (i > min_index)
+			num_out[i] = num_in[i];
+	}
+	if (num_out[1] > num_out[2]) {
+		int temp;
+		temp = num_out[1];
+		num_out[1] = num_out[2];
+		num_out[2] = temp;
+	}
+	printf("%d ", num_out[1]);
+	return 0;
+}
