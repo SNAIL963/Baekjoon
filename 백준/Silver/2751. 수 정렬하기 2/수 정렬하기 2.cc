afc1ff77d6e7a20bcebb0d36fc5ac76d7dfbd51b
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2023-02-21 02:41:56 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 388 ms, Memory: 8180 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int num;
+	scanf("%d", &num);
+
+	vector <int> vector1;
+
+	for (int i = 0; i < num; i++) {
+		int a;
+		scanf("%d", &a);
+		vector1.push_back(a);
+	}
+	sort(vector1.begin(), vector1.end());
+
+	for (int i = 0; i < num; i++) {
+		printf("%d\n", vector1[i]);
+	}
+}
