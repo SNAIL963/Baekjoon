aa98081a5ee5e9dd789d6fd052fb6ccaa721fc84
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2023-02-21 02:46:46 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+int main(void) {
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군");
+
+	return 0;
+}
