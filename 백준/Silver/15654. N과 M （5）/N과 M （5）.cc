eab30d73b807bd2049d474dc084ae73d8a7e46f9
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2023-02-21 03:14:54 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int visited[9];
+int arr[9];
+vector <int> ans;
+
+void go() {
+	if (ans.size() == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", ans[i]);
+		}
+		printf("\n");
+		return;
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			if (visited[i] == 0) {
+				visited[i] = 1;
+				ans.push_back(arr[i]);
+				go();
+				visited[i] = 0;
+				ans.pop_back();
+			}
+		}
+	}
+}
+int main(void) {
+	scanf("%d %d", &n, &m);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	sort(&arr[1], &arr[1] + n);
+	go();
+
+	return 0;
+}
