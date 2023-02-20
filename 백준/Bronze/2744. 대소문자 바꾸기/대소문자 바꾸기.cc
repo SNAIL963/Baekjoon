0c38cb34fb14d9ce5b0d5b403170791edb71c661
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2023-02-21 03:17:25 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main() {
+	char str[101];
+	scanf("%s", str);
+
+	for (int i = 0; str[i] != 0; i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	printf("%s", str);
+}
