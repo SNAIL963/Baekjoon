f7c72cf6315a1da6a316c53bddb6e26d1306e554
민성현 <125279576+SNAIL963@users.noreply.github.com>
2023-03-27 03:32:06 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+// 파일을 읽어오기 위한 filesystem 모듈 사용
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// 윈도우에서는 터미널에서 입력을 받을 수 없어서 input txt 파일을 읽어오는 식으로 입력을 받음
 let input = fs.readFileSync(filePath).toString().split(" ");
+// 입력이 띄어쓰기를 구분자로 input에 배열의 형태로 저장된다.
 
 var a = parseInt(input[0]);
 var b = parseInt(input[1]);
