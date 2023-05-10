ff5bf64413ed9af11d3ca0757a3b3ac046a7b5c0
민성현 <125279576+SNAIL963@users.noreply.github.com>
2023-05-11 02:40:06 +0900

    [Bronze V] Title: A+B - 8, Time: 128 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let num = +input[0];
+let answer = "";
+
+for (let i = 1; i <= num; i++) {
+    let newArr = input[i].split(" ").map(item => Number(item));
+    console.log("Case #" + i + ": " + newArr[0] + " + " + newArr[1] + " = " + (newArr[0] + newArr[1]));
+}
