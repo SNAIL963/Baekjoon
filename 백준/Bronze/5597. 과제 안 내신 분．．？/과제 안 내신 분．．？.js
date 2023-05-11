728f5c0ed30f9d99a6aab70355f223997ca66198
민성현 <125279576+SNAIL963@users.noreply.github.com>
2023-05-11 12:09:37 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -3,9 +3,6 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 let arr = new Array(30);
-let newArr = input[0].split(" ").map(item => Number(item));
-let basket = +newArr[0];
-let M = +newArr[1];
 
 for (let i = 0; i < 28; i++) {
     let a = +input[i];
