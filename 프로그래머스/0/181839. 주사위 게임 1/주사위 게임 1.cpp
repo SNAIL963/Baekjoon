901026c1bba03e859099d0053c8a464203592652
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2024-10-11 17:15:56 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -7,7 +7,7 @@ int solution(int a, int b) {
     int answer = 0;
     
     if(a % 2 == 1 && b % 2 == 1) answer = a * a + b * b;
-    else if(a % 2 == 0 && b % 2 == 0) a > b ? answer = a - b : answer = b - a;
+    else if(a % 2 == 0 && b % 2 == 0) answer = a > b ? a - b : b - a;
     else answer = (a + b) * 2;
     
     return answer;
