01d47df3d0fd68eeb4a668a46572fd79407740f4
SNAIL963 <125279576+SNAIL963@users.noreply.github.com>
2024-10-17 22:38:08 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(int i = 0; i < myString.length(); i++){
+        if(myString[i] == 'a' || myString[i] == 'A') {
+            answer += toupper(myString[i]);
+        }
+        else {
+            answer += tolower(myString[i]);
+        }
+    }
+    return answer;
+}
